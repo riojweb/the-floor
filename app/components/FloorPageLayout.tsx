177af4b55e75a8f1623b01f5fce55a236feed30b
Cameron Pavao <cam9548@gmail.com>
2025-12-21 09:48:40 -0600

    Refactor FloorPageLayout to use min-h-screen and remove unnecessary overflow-hidden (#2)

--- app/components/FloorPageLayout.tsx
+++ app/components/FloorPageLayout.tsx
@@ -6,11 +6,9 @@ interface FloorPageLayoutProps {
 
 export default function FloorPageLayout({ children }: FloorPageLayoutProps) {
   return (
-    <main className="relative h-screen bg-black text-white flex flex-col items-center justify-center overflow-hidden">
+    <main className="relative min-h-screen bg-black text-white flex flex-col items-center justify-center overflow-x-hidden">
       <div className="absolute inset-0 grid-background pointer-events-none"></div>
-      <div className="relative z-10 w-full h-full overflow-hidden">
-        {children}
-      </div>
+      <div className="relative z-10 w-full">{children}</div>
     </main>
   );
 }
